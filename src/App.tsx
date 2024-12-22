7d73f9f91bc45bf572b036c5ea23c942ab1ff729
Cydo <cydoentis@gmail.com>
2024-12-21 20:32:29 -0500

    Added drag and drop library and lucide icons.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
-import { Container, Text } from "@mantine/core";
-
+import {
+  Anchor,
+  Badge,
+  Box,
+  Card,
+  Container,
+  Flex,
+  Group,
+  Title,
+} from "@mantine/core";
+import { DndListHandle } from "./DndListHandle";
 
 function App() {
-
-
   return (
-    <Container size="xl" style={{ border: "1px solid red" }}>
-      <Text size="xl">Working?</Text>
+    <Container size="xl" style={{ border: "1px solid red" }} p={16}>
+      <Flex justify="space-between">
+        <Title size="1.5rem">Task Garden</Title>
+
+        <Group>
+          <Anchor>Tasks</Anchor>
+          <Anchor>Garden</Anchor>
+        </Group>
+      </Flex>
+
+      <Box>
+        <Card shadow="lg" withBorder radius="lg" w={300}>
+          <Flex justify="justify-between">
+            <Group>
+              <Box maw={200}>
+                <Title order={4}>Christmas Shopping</Title>
+              </Box>
+              <Badge variant="light" color="red">
+                5d left
+              </Badge>
+            </Group>
+          </Flex>
+          <DndListHandle />
+        </Card>
+      </Box>
     </Container>
   );
 }
