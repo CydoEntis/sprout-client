7154851afee729d9bc19adaa042c9ee2bfaa377c
Cydo <cydoentis@gmail.com>
2024-12-21 20:07:53 -0500

    Added Mantine UI and all required dependencies

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Container, Text } from "@mantine/core";
+
+
+function App() {
+
+
+  return (
+    <Container size="xl" style={{ border: "1px solid red" }}>
+      <Text size="xl">Working?</Text>
+    </Container>
+  );
+}
+
+export default App;
