a765fddf14539d439394a41ab1d6d49acb39f26f
Cydo <cydoentis@gmail.com>
2025-01-14 03:21:12 -0500

    Removed unused imports

--- src/features/Tasks/TaskCard.tsx
+++ src/features/Tasks/TaskCard.tsx
@@ -1,9 +1,5 @@
-import { Card, Divider, Group, Button } from "@mantine/core";
-import { motion, AnimatePresence } from "framer-motion";
-import TaskListHeader from "./TaskListHeader";
-import TaskDateRange from "./TaskDateRange";
-import { Plus } from "lucide-react";
-import { DndListHandle } from "../../DndListHandle";
+import { Card, } from "@mantine/core";
+
 import TaskCardHeader from "./TaskCardHeader";
 import TaskCardBody from "./TaskCardBody";
 
@@ -14,8 +10,6 @@ type TaskCardProps = {
 };
 
 function TaskCard({ onOpenAddTask, isOpen, onOpenTaskList }: TaskCardProps) {
-  const startDate = new Date("2023-12-11T14:00:00");
-  const endDate = new Date("2025-02-15T09:30:00");
 
   return (
     <Card withBorder radius="lg">
