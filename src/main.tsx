877043c13b454e92c5149bff7a530a4fe5159bc0
Cydo <cydoentis@gmail.com>
2025-01-18 12:00:42 -0500

    Feat: Installed and setup tanstack router.

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { MantineProvider } from "@mantine/core";
-import App from "./App.tsx";
 
 import "./index.css";
 import "@mantine/core/styles.css";
 import theme from "./components/theme/theme.config.ts";
 
+import { routeTree } from "./routeTree.gen";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MantineProvider theme={theme}>
-      <App />
+      <RouterProvider router={router} />
     </MantineProvider>
   </StrictMode>
 );
