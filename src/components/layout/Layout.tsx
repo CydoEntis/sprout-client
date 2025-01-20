eef13fed4e4867fa4a80c620ec423198fe1eeec9
Cydo <cydoentis@gmail.com>
2025-01-20 17:34:46 -0500

    Fix: Fixed nested anchor tag error by removing nested Anchor components from custom links.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,14 @@
 import { Anchor, Box, Container, Flex, Group, Title } from "@mantine/core";
 import React from "react";
 import ThemeToggle from "../theme/ThemeToggle";
-import { Outlet } from "@tanstack/react-router";
+import { Outlet, useLocation } from "@tanstack/react-router"; // <-- useLocation here
 import useAuthStore from "../../stores/useAuthStore";
 import { CustomLink } from "../CustomLink";
 
 function Layout() {
   const { user } = useAuthStore();
+  const location = useLocation(); // Get current location (URL path)
+
   return (
     <Box bg="primary" mih="100vh">
       <Container size="md" py={16}>
@@ -16,8 +18,29 @@ function Layout() {
           <Group>
             {user ? (
               <>
-                <Anchor c="inverse">Tasks</Anchor>
-                <Anchor c="inverse">Garden</Anchor>
+                <CustomLink
+                  to="/"
+                  c="inverse"
+                  href="/tasks"
+                  style={{
+                    fontWeight:
+                      location.pathname === "/tasks" ? "bold" : "normal",
+                  }}
+                >
+                  Tasks
+                </CustomLink>
+
+                <CustomLink
+                  to="/"
+                  c="inverse"
+                  href="/tasks"
+                  style={{
+                    fontWeight:
+                      location.pathname === "/tasks" ? "bold" : "normal",
+                  }}
+                >
+                  Garden
+                </CustomLink>
                 <Anchor
                   c="inverse"
                   onClick={() => useAuthStore.getState().logoutUser()}
@@ -27,12 +50,24 @@ function Layout() {
               </>
             ) : (
               <>
-                <CustomLink to={"/login"}>
-                  <Anchor c="inverse">Login</Anchor>
+                <CustomLink
+                  to={"/login"}
+                  c="inverse"
+                  style={{
+                    fontWeight:
+                      location.pathname === "/login" ? "bold" : "normal",
+                  }}
+                >
+                  Login
                 </CustomLink>
-                <CustomLink to={"/register"}>
-                  <Anchor c="inverse">Register</Anchor>
-                </CustomLink>
+                <CustomLink
+                  to={"/register"}
+                  c="inverse"
+                  style={{
+                    fontWeight:
+                      location.pathname === "/register" ? "bold" : "normal",
+                  }}
+                >Register</CustomLink>
               </>
             )}
             <ThemeToggle />
