e1a1bffd39086edd7a6aa7e33217fe31ad9d111b
Cydo <cydoentis@gmail.com>
2025-01-20 17:40:41 -0500

    Feat: Added a check to see if an access token is present in local storage and if it is, decode it and then login the user and set their info.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,39 @@
 import * as React from "react";
-import {  createRootRoute } from "@tanstack/react-router";
+import { createRootRoute } from "@tanstack/react-router";
 import { MantineProvider } from "@mantine/core";
 import theme from "../components/theme/theme.config";
 import Layout from "../components/layout/Layout";
+import { jwtDecode } from "jwt-decode";
+import useAuthStore from "../stores/useAuthStore";
+import { DecodedToken } from "../features/auth/shared/auth.types";
 
 export const Route = createRootRoute({
   component: RootComponent,
 });
 
 function RootComponent() {
+  const { setUser, setAccessToken } = useAuthStore();
+
+  React.useEffect(() => {
+    const taskGardenData = localStorage.getItem("taskgarden");
+
+    if (taskGardenData) {
+      const parsedData = JSON.parse(taskGardenData);
+
+      if (parsedData?.accessToken) {
+        const decodedToken = jwtDecode<DecodedToken>(parsedData.accessToken);
+
+        setAccessToken(parsedData.accessToken);
+        setUser({
+          id: decodedToken.userId,
+          username: decodedToken.sub,
+          email: decodedToken.email,
+          role: "Admin",
+        });
+      }
+    }
+  }, [setAccessToken, setUser]);
+
   return (
     <React.Fragment>
       <MantineProvider theme={theme}>
