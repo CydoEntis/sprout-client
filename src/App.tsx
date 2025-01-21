94f7bba5e896ef0158e6516eff7cd87e1568bb3d
Cydo <cydoentis@gmail.com>
2025-01-20 22:54:06 -0500

    Feat: Added reusable farmprogress card to display the progress of your highest growth crop.

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,8 @@ import InProgressTaskListCard from "./features/task-list/InProgressTaskListCard"
 import CompletedTaskListCard from "./features/task-list/CompletedTaskListCard";
 import TaskListTabs from "./features/task-list-tabs/TaskListTabs";
 
-import TomatoStage3 from "./assets/crops/tomato-stage-3.png";
 import useAuthStore from "./stores/useAuthStore";
+import FarmProgress from "./features/farm/FarmProgress";
 
 function App() {
   const { user } = useAuthStore();
@@ -33,42 +33,12 @@ function App() {
     setOpenCard((prev) => (prev === cardId ? null : cardId));
   };
 
-  const stage = 3;
 
   return (
     <>
       <Stack gap={4} pb={32}>
         <Title>Welcome back, {user!.username}</Title>
-        <Group align="center" gap={4}>
-          <Text c="dimmed">Your tomatos are almost fully grown!</Text>
-          <Avatar size="sm" w={30} h={30} src={TomatoStage3} />
-        </Group>
-        <Group grow gap={5} mt="xs">
-          <Progress
-            size="xs"
-            color="lime"
-            value={stage > 0 ? 100 : 0}
-            transitionDuration={0}
-          />
-          <Progress
-            size="xs"
-            color="lime"
-            transitionDuration={0}
-            value={stage < 2 ? 0 : 100}
-          />
-          <Progress
-            size="xs"
-            color="lime"
-            transitionDuration={0}
-            value={stage < 3 ? 0 : 100}
-          />
-          <Progress
-            size="xs"
-            color="lime"
-            transitionDuration={0}
-            value={stage < 4 ? 0 : 100}
-          />
-        </Group>
+        <FarmProgress />
       </Stack>
 
       <TaskListTabs />
