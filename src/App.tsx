54f2d5a323479b5cb397b1aeb912f8d970879b5c
Cydo <cydoentis@gmail.com>
2025-01-20 23:03:05 -0500

    Refactor: Removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,4 @@
-import { useState } from "react";
-import {
-  Anchor,
-  Avatar,
-  Box,
-  Container,
-  Flex,
-  Group,
-  Modal,
-  Progress,
-  Stack,
-  Text,
-  Title,
-} from "@mantine/core";
-import TaskCard from "./features/Tasks2/TaskCard";
-import { useDisclosure } from "@mantine/hooks";
-import { taskLists } from "./features/Tasks2/data";
-import { Calendar, Divide } from "lucide-react";
-import ThemeToggle from "./components/theme/ThemeToggle";
-import InProgressTaskListCard from "./features/task-list/InProgressTaskListCard";
-import CompletedTaskListCard from "./features/task-list/CompletedTaskListCard";
+import { Stack, Title } from "@mantine/core";
 import TaskListTabs from "./features/task-list-tabs/TaskListTabs";
 
 import useAuthStore from "./stores/useAuthStore";
@@ -26,13 +6,6 @@ import FarmProgress from "./features/farm/FarmProgress";
 
 function App() {
   const { user } = useAuthStore();
-  const [opened, { open, close }] = useDisclosure(false);
-  const [openCard, setOpenCard] = useState<number | null>(null);
-
-  const handleToggleCard = (cardId: number) => {
-    setOpenCard((prev) => (prev === cardId ? null : cardId));
-  };
-
 
   return (
     <>
@@ -40,7 +13,6 @@ function App() {
         <Title>Welcome back, {user!.username}</Title>
         <FarmProgress />
       </Stack>
-
       <TaskListTabs />
     </>
   );
