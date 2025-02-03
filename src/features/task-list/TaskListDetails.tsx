c9611a9e9c4d3f2b69809349b4db61749bc54bde
Cydo <cydoentis@gmail.com>
2025-02-03 16:39:00 -0500

    Refactor: Updated design of the task list details component

--- src/features/task-list/TaskListDetails.tsx
+++ src/features/task-list/TaskListDetails.tsx
@@ -1,7 +1,17 @@
-import { Paper } from "@mantine/core";
+import {
+  Paper,
+  Title,
+  Text,
+  Stack,
+  Badge,
+  Group,
+  Button,
+  Avatar,
+} from "@mantine/core";
 import TaskListHeader from "../Tasks2/TaskListHeader";
 import { DndListHandle } from "../../DndListHandle";
 import AddTaskBtn from "../Tasks2/AddTaskBtn";
+import { Clock, Plus } from "lucide-react";
 
 type TaskListDetailsProps = {
   onOpenAddTask: () => void;
@@ -9,10 +19,50 @@ type TaskListDetailsProps = {
 
 function TaskListDetails({ onOpenAddTask }: TaskListDetailsProps) {
   return (
-    <Paper>
-      <TaskListHeader />
+    <Paper p={16} radius="md" mt={16}>
+      <Stack gap={2} mb={16}>
+        <Group justify="space-between" align="center">
+          <Title>Website for Rune.io</Title>
+          <Badge color="yellow.4" variant="light">
+            In Progress
+          </Badge>
+        </Group>
+        <Text c="dimmed">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui laborum
+          nemo rerum obcaecati quidem tempore iusto amet doloremque nisi odit!
+          Aut excepturi ipsam ipsa! Doloribus eveniet cumque dolores voluptate
+          excepturi?
+        </Text>
+        <Group gap={8}>
+          <Button size="xs" variant="light" color="lime">
+            Details
+          </Button>
+          <Button size="xs" variant="subtle" color="inverse">
+            Comments
+          </Button>
+          <Button size="xs" variant="subtle" color="inverse">
+            Attachments
+          </Button>
+        </Group>
+        <Stack mt={16} gap={4}>
+          <Text size="xs">Members</Text>
+          <Group justify="space-between" align="center">
+            <Avatar.Group>
+              <Avatar color="initials" name="Bilbo Baggins" />
+              <Avatar color="initials" name="Tito Teets" />
+              <Avatar color="initials" name="Zoro Boro" />
+              <Avatar>
+                <Plus size={20} />
+              </Avatar>
+            </Avatar.Group>
+            <Badge size="lg" leftSection={<Clock size={16} />} variant="light" color="gray">
+              Deadline: February 6
+            </Badge>
+          </Group>
+        </Stack>
+      </Stack>
+      {/* <TaskListHeader /> */}
       <DndListHandle />
-      <AddTaskBtn onOpenAddTask={onOpenAddTask} />
     </Paper>
   );
 }
