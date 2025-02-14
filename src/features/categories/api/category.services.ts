fb3bb7bf1251db0f217bf0dddefef3f26a3279a6
Cydo <cydoentis@gmail.com>
2025-02-14 14:53:35 -0500

    Added a delete category service method

--- src/features/categories/api/category.services.ts
+++ src/features/categories/api/category.services.ts
@@ -9,11 +9,8 @@ import {
   UpdateCategoryRequest,
 } from "../shared/category.types";
 
-export const createCategory = async (
-  newCategory: NewCategoryRequest
-): Promise<NewCategoryResponse> => {
-  const response = (await apiClient.post(`${endpoints.category}`, newCategory))
-    .data;
+export const createCategory = async (newCategory: NewCategoryRequest): Promise<NewCategoryResponse> => {
+  const response = (await apiClient.post(`${endpoints.category}`, newCategory)).data;
   if (!response.success) throw new Error(response.message);
   return response.data;
 };
@@ -24,21 +21,20 @@ export const getAllCategories = async (): Promise<CategoryResponse[]> => {
   return response.data;
 };
 
-export const getAllTaskListsForCategory = async (
-  category: string
-): Promise<TaskListResponse[]> => {
-  const response = (await apiClient.get(`${endpoints.category}/${category}`))
-    .data;
+export const getAllTaskListsForCategory = async (category: string): Promise<TaskListResponse[]> => {
+  const response = (await apiClient.get(`${endpoints.category}/${category}`)).data;
   if (!response.success) throw new Error(response.message);
   return response.data;
 };
 
-export const updateCategory = async (
-  updatedCategory: UpdateCategoryRequest
-): Promise<UpdateCategoryResponse> => {
-  const response = (
-    await apiClient.put(`${endpoints.category}`, updatedCategory)
-  ).data;
+export const updateCategory = async (updatedCategory: UpdateCategoryRequest): Promise<UpdateCategoryResponse> => {
+  const response = (await apiClient.put(`${endpoints.category}`, updatedCategory)).data;
+  if (!response.success) throw new Error(response.message);
+  return response.data;
+};
+
+export const deleteCategory = async (categoryId: number): Promise<DeleteCategoryResponse> => {
+  const response = (await apiClient.delete(`${endpoints.category}/${categoryId}`)).data;
   if (!response.success) throw new Error(response.message);
   return response.data;
 };
