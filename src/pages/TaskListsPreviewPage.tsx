b02913aefa8d58761b421ca1e780bbfd87fee1ef
Cydo <cydoentis@gmail.com>
2025-03-01 12:05:21 -0500

    Fix: Added missing key to taskListCards for InProgressTaskListCards on TaskListPage, and corrected incorrect props for UpsertTaskListModal

--- src/pages/TaskListsPreviewPage.tsx
+++ src/pages/TaskListsPreviewPage.tsx
@@ -1,6 +1,6 @@
 import { useDisclosure } from "@mantine/hooks";
 import { TaskList } from "../features-new/task-list/shared/task-list.types";
-import CreateTaskListModal from "../features-new/task-list/components/UpsertTaskListModal";
+import UpsetTaskListModal from "../features-new/task-list/components/UpsertTaskListModal";
 import GridList from "../components/GridList";
 import InProgressTaskListCard from "../features-new/task-list/components/InProgressTaskListCard";
 import { Box } from "@mantine/core";
@@ -14,11 +14,11 @@ function TaskListPage({ taskLists }: TaskListPage) {
 
   return (
     <>
-      <CreateTaskListModal onClose={onCloseNewList} isOpened={isNewTaskListOpened} />
+      <UpsetTaskListModal onClose={onCloseNewList} isOpen={isNewTaskListOpened} />
       <Box mt={32}>
         <GridList>
           {taskLists.map((taskList) => (
-            <InProgressTaskListCard taskList={taskList} />
+            <InProgressTaskListCard key={taskList.id} taskList={taskList} />
           ))}
         </GridList>
       </Box>
