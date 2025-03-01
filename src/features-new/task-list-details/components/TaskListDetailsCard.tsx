12235bcb9f0809125c9bbaee7d1c0458c1f307ee
Cydo <cydoentis@gmail.com>
2025-03-01 12:10:36 -0500

    Fix: Moved UpsertTaskListModal to TaskListDetails

--- src/features-new/task-list-details/components/TaskListDetailsCard.tsx
+++ src/features-new/task-list-details/components/TaskListDetailsCard.tsx
@@ -2,6 +2,9 @@ import { Paper, Title, Text, Stack, Badge, Group, Button, Avatar } from "@mantin
 import { DndListHandle } from "../../../DndListHandle";
 import { Plus } from "lucide-react";
 import { TaskListDetails } from "../shared/task-list-details.types";
+import UpdateAndDeleteMenu from "../../../components/menus/UpdateAndDeleteMenu";
+import { useDisclosure } from "@mantine/hooks";
+import UpsertTaskListModal from "../../task-list/components/UpsertTaskListModal";
 
 type TaskListDetailsCardProps = {
   onOpenAddTask: () => void;
@@ -9,49 +12,51 @@ type TaskListDetailsCardProps = {
 };
 
 function TaskListDetailsCard({ taskListDetails }: TaskListDetailsCardProps) {
-  console.log(taskListDetails);
+  const [isUpsertTaskListOpened, { open: onOpenNewList, close: onCloseNewList }] = useDisclosure(false);
 
   return (
-    <Paper p={16} radius="md" mt={16}>
-      <Stack gap={2} mb={16}>
-        <Group justify="space-between" align="center">
-          <Title>{taskListDetails.name}</Title>
-          <Badge color="yellow.4" variant="light">
-            In Progress
-          </Badge>
-        </Group>
-        <Text c="dimmed">{taskListDetails.description}</Text>
-        <Group gap={8}>
-          <Button size="xs" variant="light" color="lime">
-            Details
-          </Button>
-          <Button size="xs" variant="subtle" color="inverse">
-            Comments
-          </Button>
-          <Button size="xs" variant="subtle" color="inverse">
-            Attachments
-          </Button>
-        </Group>
-        <Stack mt={16} gap={4}>
-          <Text size="xs">Members</Text>
+    <>
+      <UpsertTaskListModal onClose={onCloseNewList} isOpen={isUpsertTaskListOpened} />
+
+      <Paper p={16} radius="md" mt={16}>
+        <Stack gap={2} mb={16}>
           <Group justify="space-between" align="center">
-            <Avatar.Group>
-              {taskListDetails.members.map((member) => (
-                <Avatar color="initials" name={member.name} />
-              ))}
-              <Avatar>
-                <Plus size={20} />
-              </Avatar>
-            </Avatar.Group>
-            <Button variant="subtle" color="dimmed" leftSection={<Plus size={20} />}>
-              Add Item
+            <Title>{taskListDetails.name}</Title>
+            <UpdateAndDeleteMenu onUpdate={() => {}} onDelete={() => {}} />
+          </Group>
+          <Text c="dimmed">{taskListDetails.description}</Text>
+          <Group gap={8}>
+            <Button size="xs" variant="light" color="lime">
+              Details
+            </Button>
+            <Button size="xs" variant="subtle" color="inverse">
+              Comments
+            </Button>
+            <Button size="xs" variant="subtle" color="inverse">
+              Attachments
             </Button>
           </Group>
+          <Stack mt={16} gap={4}>
+            <Text size="xs">Members</Text>
+            <Group justify="space-between" align="center">
+              <Avatar.Group>
+                {taskListDetails.members.map((member) => (
+                  <Avatar color="initials" name={member.name} />
+                ))}
+                <Avatar>
+                  <Plus size={20} />
+                </Avatar>
+              </Avatar.Group>
+              <Button variant="subtle" color="dimmed" leftSection={<Plus size={20} />}>
+                Add Item
+              </Button>
+            </Group>
+          </Stack>
         </Stack>
-      </Stack>
-      {/* <TaskListHeader /> */}
-      <DndListHandle />
-    </Paper>
+        {/* <TaskListHeader /> */}
+        <DndListHandle />
+      </Paper>
+    </>
   );
 }
 
--- src/pages/TaskListsPreviewPage.tsx
+++ src/pages/TaskListsPreviewPage.tsx
@@ -1,6 +1,4 @@
-import { useDisclosure } from "@mantine/hooks";
 import { TaskList } from "../features-new/task-list/shared/task-list.types";
-import UpsertTaskListModal from "../features-new/task-list/components/UpsertTaskListModal";
 import GridList from "../components/GridList";
 import InProgressTaskListCard from "../features-new/task-list/components/InProgressTaskListCard";
 import { Box } from "@mantine/core";
@@ -10,20 +8,14 @@ type TaskListPage = {
 };
 
 function TaskListPage({ taskLists }: TaskListPage) {
-  const [isNewTaskListOpened, { open: onOpenNewList, close: onCloseNewList }] = useDisclosure(false);
-
   return (
-    <>
-      <UpsertTaskListModal onClose={onCloseNewList} isOpen={isNewTaskListOpened} />
-      <Box mt={32}>
-        <GridList>
-          {taskLists.map((taskList) => (
-            <InProgressTaskListCard key={taskList.id} taskList={taskList} />
-          ))}
-        </GridList>
-      </Box>
-      {/* <TaskListsPreview onOpen={onOpenNewList} taskLists={taskLists} /> */}
-    </>
+    <Box mt={32}>
+      <GridList>
+        {taskLists.map((taskList) => (
+          <InProgressTaskListCard key={taskList.id} taskList={taskList} />
+        ))}
+      </GridList>
+    </Box>
   );
 }
 
