02d91575d831a3c08edbda13c995be126bb36b89
Cydo <cydoentis@gmail.com>
2025-03-05 16:01:55 -0500

    Feat: Task list items now have a line through the text on a completed task.

--- src/features-new/task-list-details/components/list-item/ListItem.tsx
+++ src/features-new/task-list-details/components/list-item/ListItem.tsx
@@ -15,8 +15,12 @@ function ListItem({ item, onDelete, onChange }: ListItemProps) {
     <Paper p={8} bg="item" className={styles["list-item"]} withBorder>
       <Flex justify="space-between">
         <Group>
-          <Checkbox checked={item.isCompleted} onChange={(event) => onChange(item.id, event.currentTarget.checked)} color="lime"/>
-          <Text>{item.description}</Text>
+          <Checkbox
+            checked={item.isCompleted}
+            onChange={(event) => onChange(item.id, event.currentTarget.checked)}
+            color="lime"
+          />
+          <Text className={item.isCompleted ? styles.completed : ""}>{item.description}</Text>
         </Group>
         <ActionIcon color="red" variant="light" onClick={() => onDelete(item.id)}>
           <Trash size={20} />
