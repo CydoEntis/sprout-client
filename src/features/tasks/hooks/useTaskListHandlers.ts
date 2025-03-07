bf3f28eda05d0a41d9d250a97c6dfb7f24fa004b
Cydo <cydoentis@gmail.com>
2025-03-07 02:18:05 -0500

    Refactor: Moved task list mutation/querying logic to useTaskListHandlers hook.

---
+++ src/features/tasks/hooks/useTaskListHandlers.ts
@@ -0,0 +1,16 @@
+import { useNavigate, useParams } from "@tanstack/react-router";
+import { useDeleteTaskListMutation } from "../services/delete-task-list.service";
+import { TaskListDetails } from "../../task-list-details/shared/task-list-details.types";
+
+export function useTaskListHandlers(taskListDetails: TaskListDetails) {
+  const { categoryName } = useParams({ from: "/_authenticated/categories/$categoryName_/$taskListId" });
+  const navigate = useNavigate();
+  const deleteTaskList = useDeleteTaskListMutation();
+
+  const onDeleteTaskList = async () => {
+    await deleteTaskList.mutateAsync(taskListDetails.id);
+    navigate({ to: `/categories/${categoryName}` });
+  };
+
+  return { onDeleteTaskList };
+}
