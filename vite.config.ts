de2c0ab4478e96244c929b2a1fafc23bb764ca64
Cydo <cydoentis@gmail.com>
2025-04-14 21:13:58 -0400

    Refactor: updated git ignore.

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-// import fs from 'fs';
-// import path from 'path';
+import path from "path";
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    // https: {
-    //   key: fs.readFileSync(
-    //     path.resolve(__dirname, "./certs/localhost-key.pem")
-    //   ),
-    //   cert: fs.readFileSync(path.resolve(__dirname, "./certs/localhost.pem")),
-    // },
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "ssl/private.key")),
+      cert: fs.readFileSync(path.resolve(__dirname, "ssl/server.crt")),
+    },
+    host: true,
     port: 5173,
   },
   plugins: [react(), TanStackRouterVite()],
